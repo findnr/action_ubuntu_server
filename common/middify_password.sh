05d0ac7554dce02e20385948e6d9a97efe5f2eba
findnr <cym504875043@gmail.com>
2024-03-01 10:14:01 +0800

    Update middify_password.sh

--- common/middify_password.sh
+++ common/middify_password.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-pwd=1
-if [ $# -eq 0 ]; then
-    echo "No arguments provided."
-else
-    echo "Number of arguments: $#"
-    echo "info: $1"
-    pwd=$1
-fi
+pwd=123456
+# if [ $# -eq 0 ]; then
+#     echo "No arguments provided."
+# else
+#     echo "Number of arguments: $#"
+#     echo "info: $1"
+#     pwd=$1
+# fi
 
-echo $pwd
+echo "root runner user password is:$pwd"
 
 expect -c "
     spawn sudo passwd runner
