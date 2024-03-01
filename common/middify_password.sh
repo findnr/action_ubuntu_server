3b8457fa64f470d6bc8dabb6e543b515402a5752
findnr <cym504875043@gmail.com>
2024-03-01 09:26:42 +0800

    Create middify_password.sh

---
+++ common/middify_password.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+pwd=1
+if [ $# -eq 0 ]; then
+    echo "No arguments provided."
+else
+    echo "Number of arguments: $#"
+    echo "info: $1"
+    pwd=$1
+fi
+
+expect -c "
+    spawn sudo passwd runner
+    expect {
+        \"password\" { send \"$pwd\r\";}
+    }
+    expect {
+        \"password\" { send \"$pwd\r\";}
+    }
+expect eof"
+expect -c "
+    spawn sudo passwd root
+    expect {
+        \"password\" { send \"$pwd\r\";}
+    }
+    expect {
+        \"password\" { send \"$pwd\r\";}
+    }
+expect eof"
