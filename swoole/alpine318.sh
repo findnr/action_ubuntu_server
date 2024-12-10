f557354dcc5c38f14faf09723e1e8b3e00ea49e7
findnr <cym504875043@gmail.com>
2024-12-10 09:56:35 +0800

    Update alpine318.sh

--- swoole/alpine318.sh
+++ swoole/alpine318.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker pull alpine:3.18
+set -e
 cd /mnt
 git clone --recursive https://github.com/swoole/swoole-cli.git
 cd swoole-cli
@@ -7,13 +7,34 @@ bash setup-php-runtime.sh
 composer install
 php prepare.php
 php prepare.php +inotify +mongodb +xlswriter
-docker run -ti --name swoole-cli-mian -v /mnt/swoole-cli:/work alpine:3.18 /bin/sh
-cd /work
-sh setup-php-runtime.sh
-export PATH=$PATH:/work/bin/runtime
-sh  sapi/quickstart/linux/alpine-init.sh
-php prepare.php +inotify +mongodb +xlswriter
-./make.sh all-library
-./make.sh config
-./make.sh build
-./make.sh archive
+
+# 定义基础镜像和容器名称
+BASE_IMAGE="alpine:3.18"
+CONTAINER_NAME="swoole-cli-main318"
+
+# 定义在容器内执行的命令
+CONTAINER_COMMANDS="
+cd /work &&
+sh setup-php-runtime.sh &&
+export PATH=\$PATH:/work/bin/runtime &&
+sh sapi/quickstart/linux/alpine-init.sh &&
+php prepare.php +inotify +mongodb +xlswriter &&
+./make.sh all-library &&
+./make.sh config &&
+./make.sh build &&
+./make.sh archive &&
+exit
+"
+
+# 检查容器是否存在
+if docker ps -a --format "{{.Names}}" | grep -q "^${CONTAINER_NAME}$"; then
+  echo "容器 ${CONTAINER_NAME} 已存在，直接进入执行命令..."
+  docker exec -it $CONTAINER_NAME /bin/sh -c "$CONTAINER_COMMANDS"
+else
+  echo "容器 ${CONTAINER_NAME} 不存在，创建新容器并执行命令..."
+  docker pull $BASE_IMAGE
+  docker run -dit --name $CONTAINER_NAME -v /mnt/swoole-cli:/work $BASE_IMAGE /bin/sh
+  docker exec -it $CONTAINER_NAME /bin/sh -c "$CONTAINER_COMMANDS"
+fi
+
+
